e90ee70e00b55625bdd9f06eb43811b80f436b3a
iudizm <43123002+iudizm@users.noreply.github.com>
2019-06-27 21:39:58 -0300

    Add files via upload

--- funx.py
+++ funx.py
@@ -7,7 +7,12 @@ def cadastrar(nome,telefone):
     arq.write(telefone)
     arq.write('\n')
     arq.close()
-    
+    backup = open('lista_telefonica_backup.txt','a')
+    backup.write(nome)
+    backup.write(' - ')
+    backup.write(telefone)
+    backup.write('\n')
+    backup.close()
 
 def listar():
     arq = open('lista_telefonica.txt','r')
@@ -15,3 +20,13 @@ def listar():
         linha = linha.rstrip() #tira uma linha em branco causada pelo print
         print(linha)
     arq.close()
+
+def apagar(telefone):
+    with open('lista_telefonica.txt','r+') as arquivo:
+        novo_arq = arquivo.readlines()
+        arquivo.seek(0)
+        for linha in novo_arq:
+            if telefone not in linha:
+                arquivo.write(linha)
+        arquivo.truncate()
+
--- main.py
+++ main.py
@@ -3,17 +3,40 @@ import os
 from funx import *
 
 print(cabecalho())
-print("\n=== AGENDA TELEFÔNICA DO NOKIA TIJOLÃO ===")
+print("\n=== AGENDA TELEFÔNICA ===")
 
-escolha = input('\n ===MENU===\n 1 - add telefone\n 2 - listar telefones\n 3 - apagar telefone\n 0 - SAIR\n\n DIGITE SUA OPÇÃO: ')
+while True:
 
-if escolha == '1':
-    nome = input("Nome do contato: ")
-    telefone = input('Telefone do contato: ')
-    cadastrar(nome, telefone)
+    escolha = input('\n ===MENU===\n 1 - add telefone\n 2 - listar telefones\n 3 - apagar telefone\n 0 - SAIR\n\n DIGITE SUA OPÇÃO: ')
 
-if escolha == '2':
-    try:
-        listar()
-    except FileNotFoundError:
-        print('nao existe lista telefonica ainda!')
+    if escolha == '1':
+        try:
+            nome = input("Nome do contato: ")
+            telefone = input('Telefone do contato(9 algarismos): ')
+            cadastrar(nome, telefone)
+        except TypeError:
+            print("Erro de digitação, tente novamente")
+
+    elif escolha == '2':
+        try:
+            listar()
+        except FileNotFoundError:
+            print('nao existe lista telefonica ainda!\n Adicione um contato para que ele seja listado!')
+
+    elif escolha == '3':
+        
+            telefone = input('\nPara que não ocorram erros, por favor digite o número a ser apagado\nCertifique-se de digitar conforme o número na listagem:  ')
+            apagar(telefone)
+            print('\nCONTATO APAGADO COM SUCESSO')
+    elif escolha == '0':
+        sair = input('DESEJA SAIR DO PROGRAMA??? (S/N)')
+        
+        if sair.lower() == 's':
+            break
+        elif sair.lower() == 'n':
+            print('ok entao')
+        else:
+            print('Por favor, (S/N)')
+
+    else:
+        print('Digite uma opção válida')
